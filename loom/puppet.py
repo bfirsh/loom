df1f84f862e791f30580e1e4147d0212775c1a9a
Ben Firshman <ben@firshman.co.uk>
2012-10-06 13:41:09 +0100

    Added puppet.update_configs command

--- loom/puppet.py
+++ loom/puppet.py
@@ -16,17 +16,37 @@ def get_puppetmaster_host():
 @task
 def update():
     """
-    Upload puppet modules and manifests
+    Upload puppet modules
     """
     # Install local modules
     upload_dir('modules/', '/etc/puppet/modules', use_sudo=True)
+
     # Install vendor modules
     put('Puppetfile', '/etc/puppet/Puppetfile', use_sudo=True)
     with cd('/etc/puppet'):
         sudo('librarian-puppet install --path /etc/puppet/vendor')
 
+@task
+def update_configs():
+    """
+    Upload puppet configs and manifests
+    """
+    # Set role
+    # TODO: this should be done with facts.d, but it was a pain in the arse to get it 
+    # working
+    put(StringIO(current_role()), '/etc/puppet/role', use_sudo=True)
+
+    # Upload Puppet configs
+    upload_template(os.path.join(files_path, 'puppet/puppet.conf'), '/etc/puppet/puppet.conf', {
+        'server': get_puppetmaster_host(),
+        'environment': env.environment,
+    }, use_sudo=True)
+    put(os.path.join(files_path, 'puppet/auth.conf'), '/etc/puppet/auth.conf', use_sudo=True)
+
+    # Install manifest
+    sudo('mkdir -p /etc/puppet/manifests')
     put(StringIO('include "roles::$role"\n'), '/etc/puppet/manifests/site.pp', use_sudo=True)
-    
+
 
 @task
 def install_master():
@@ -34,7 +54,6 @@ def install_master():
     Install puppetmaster, update its modules and install agent.
     """
     execute(install_agent)
-    execute(update)
     put(os.path.join(files_path, 'init/puppetmaster.conf'), '/etc/init/puppetmaster.conf', use_sudo=True)
     restart('puppetmaster')
 
@@ -53,14 +72,10 @@ def install_agent():
     # http://docs.puppetlabs.com/guides/installation.html
     sudo('puppet resource group puppet ensure=present')
     sudo("puppet resource user puppet ensure=present gid=puppet shell='/sbin/nologin'")
-    sudo('mkdir -p /etc/puppet')
-    put(StringIO(current_role()), '/etc/puppet/role', use_sudo=True)
-    upload_template(os.path.join(files_path, 'puppet/puppet.conf'), '/etc/puppet/puppet.conf', {
-        'server': get_puppetmaster_host(),
-        'environment': env.environment,
-    }, use_sudo=True)
-    put(os.path.join(files_path, 'puppet/auth.conf'), '/etc/puppet/auth.conf', use_sudo=True)
     put(os.path.join(files_path, 'init/puppet.conf'), '/etc/init/puppet.conf', use_sudo=True)
+    sudo('mkdir -p /etc/puppet')
+    execute(update_configs)
+    execute(update)
     restart('puppet')
 
 @task
@@ -75,7 +90,7 @@ def force():
     """
     Force puppet agent run
     """
-    sudo('HOME=/root FACTER_role=%s puppet agent --onetime --no-daemonize --verbose' % (current_role(), get_puppetmaster_host()))
+    sudo('HOME=/root FACTER_role=%s puppet agent --onetime --no-daemonize --verbose --waitforcert 5' % current_role())
 
 
 
