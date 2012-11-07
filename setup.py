7a8baffd2436e9100acd7cfeb4ab0f320134b20c
Ben Firshman <ben@firshman.co.uk>
2012-11-07 19:39:22 +0000

    Version 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='loom',
-    version='0.0.6',
+    version='0.0.7',
     description='Elegant deployment with Fabric and Puppet.',
     author='Ben Firshman',
     author_email='ben@firshman.co.uk',
