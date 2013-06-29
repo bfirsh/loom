a3d0c4ef573f36523d2ae1cd4f61e927a2114eae
Ben Firshman <ben@firshman.co.uk>
2013-06-29 19:31:31 +0100

    Spacing

--- spec/config_spec.py
+++ spec/config_spec.py
@@ -4,7 +4,6 @@ from fabric.api import env
 from loom.config import host_roles, current_roles
 
 with describe('loom.config.host_roles'):
-
     def it_returns_the_role_for_a_host_with_a_single_role():
         env.roledefs = {'app': 'app.example.com'}
         assert host_roles('app.example.com') == ['app']
@@ -29,8 +28,11 @@ with describe('loom.config.host_roles'):
         assert host_roles('app1.example.com') == ['app', 'db']
         assert host_roles('app2.example.com') == ['app', 'db']
 
+
 with describe('loom.config.current_roles'):
     def it_returns_the_roles_for_the_current_host():
         env.roledefs = {'app': 'app.example.com'}
         env.host_string = 'app.example.com'
         assert current_roles() == ['app']
+
+
